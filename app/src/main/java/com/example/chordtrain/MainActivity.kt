d55145b535d32ccf7112c2bddea54fb360fd2d53
Dfej <dfej@itu.dk>
2025-03-24 14:28:56 +0100

    test push

--- app/src/main/java/com/example/chordtrain/MainActivity.kt
+++ app/src/main/java/com/example/chordtrain/MainActivity.kt
@@ -16,32 +16,5 @@ import com.example.chordtrain.ui.theme.ChordTrainTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ChordTrainTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChordTrainTheme {
-        Greeting("Android")
     }
 }
