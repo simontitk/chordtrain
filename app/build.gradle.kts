aeb268c9d0941fbaba8998df3ea681f7baff55c7
simontitk <simontitk@gmail.com>
2025-03-31 20:23:10 +0200

    update dependencies for ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
-
-android {
+    android {
     namespace = "com.example.chordtrain"
     compileSdk = 35
 
@@ -40,7 +40,10 @@ android {
 }
 
 dependencies {
-
+    val roomVersion = "2.6.1" // Use the latest version
+    implementation("androidx.room:room-runtime:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion") // Use ksp instead of kapt
+    implementation("androidx.room:room-ktx:$roomVersion")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,12 +52,14 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
+    implementation(libs.androidx.constraintlayout)
+    implementation (libs.androidx.room.runtime)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
