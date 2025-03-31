c34d329a557a5b062fbaaf6590d08655c0d456d4
simontitk <simontitk@gmail.com>
2025-03-31 20:22:02 +0200

    add database classes

---
+++ app/src/main/java/com/example/chordtrain/MusicalKey.kt
@@ -0,0 +1,18 @@
+package com.example.chordtrain
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "musical_key")
+data class MusicalKey(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val chord1: String,
+    val chord2: String,
+    val chord3: String,
+    val chord4: String,
+    val chord5: String,
+    val chord6: String,
+    val chord7: String
+)
---
+++ app/src/main/java/com/example/chordtrain/MusicalKeyDao.kt
@@ -0,0 +1,17 @@
+package com.example.chordtrain
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface MusicalKeyDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: MusicalKey)
+
+    @Query("SELECT * FROM musical_key ORDER BY name DESC")
+    fun getAllMusicalKey(): LiveData<List<MusicalKey>>
+}
