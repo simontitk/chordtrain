2065b29bde7895d2c88398e5bad4eff3e0923f7f
simontitk <simontitk@gmail.com>
2025-04-18 01:16:49 +0200

    create playviewmodel and its factory

--- app/src/main/java/com/example/chordtrain/PlayViewModel.kt
+++ app/src/main/java/com/example/chordtrain/PlayViewModel.kt
@@ -1,2 +1,35 @@
-package com.example.chordtrain 
+package com.example.chordtrain
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.chordtrain.audio.ChordSequenceGenerator
+import com.example.chordtrain.db.MusicalKey
+
+class PlayViewModel(
+    private val selectedLength: Int,
+    private val selectedDifficulty: String,
+    private val selectedMusicalKey: MusicalKey
+): ViewModel() {
+
+    private val generator = ChordSequenceGenerator(selectedMusicalKey, selectedDifficulty, selectedLength)
+    val hasAnswered = MutableLiveData(false)
+    var trueChordSequence = generator.getChordSequence()
+
+    fun checkAnswer() {
+
+        hasAnswered.value = true
+        // TODO: checking logic
+        // TODO: save record to database
+    }
+
+    fun skipSequence() {
+        // TODO: save record to database
+        generateNextSequence()
+    }
+
+    fun generateNextSequence() {
+        hasAnswered.value = false
+        trueChordSequence = generator.getChordSequence()
+    }
+
+}
