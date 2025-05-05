f9c5180f9349f27afaeeb684dc68771bcdbf9d63
simontitk <simontitk@gmail.com>
2025-05-05 17:18:09 +0200

    refactor chordsequencegenerator

--- app/src/main/java/com/example/chordtrain/audio/ChordSequenceGenerator.kt
+++ app/src/main/java/com/example/chordtrain/audio/ChordSequenceGenerator.kt
@@ -12,29 +12,32 @@ class ChordSequenceGenerator(
     private val difficulty: String,
     private val sequenceLength: Int
 ) {
+    private var possibleChords: List<String>? = null
 
-
-    fun getChordSequence(): List<String> {
-        val chordNames = mutableListOf(musicalKey.chord1, musicalKey.chord4, musicalKey.chord5)
-
-        if (difficulty == "Medium") {
-            chordNames.add(musicalKey.chord6)
-            chordNames.add(musicalKey.chord2)
+    fun getPossibleChords(): List<String> {
+        if( possibleChords == null) {
+            val chords = mutableListOf(musicalKey.chord1, musicalKey.chord4, musicalKey.chord5)
+            if (difficulty == "Medium") {
+                chords.add(musicalKey.chord6)
+                chords.add(musicalKey.chord2)
+            }
+            if (difficulty == "Hard") {
+                chords.add(musicalKey.chord6)
+                chords.add(musicalKey.chord2)
+                chords.add(musicalKey.chord3)
+                chords.add(musicalKey.chord7)
+            }
+            possibleChords = chords
         }
+        return possibleChords!!
+    }
 
-        if (difficulty == "Hard") {
-            chordNames.add(musicalKey.chord6)
-            chordNames.add(musicalKey.chord2)
-            chordNames.add(musicalKey.chord3)
-            chordNames.add(musicalKey.chord7)
-        }
-
+    fun getRandomChordSequence(): List<String> {
+        val chords = getPossibleChords()
         val randomChords: MutableList<String> = mutableListOf()
-
         for (i in 1..sequenceLength) {
-            randomChords.add(chordNames.random())
+            randomChords.add(chords.random())
         }
-
         return randomChords
     }
 }
